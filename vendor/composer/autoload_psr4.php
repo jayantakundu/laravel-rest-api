4db0ba6c30fed64ff43a301f639066295d9e35c0
jayantakundu <jayantakundu89@gmail.com>
2015-12-06 14:45:13 +0000

    add env

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -31,6 +31,8 @@ return array(
     'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
     'Illuminate\\' => array($vendorDir . '/laravel/framework/src/Illuminate'),
     'Faker\\' => array($vendorDir . '/fzaninotto/faker/src/Faker'),
+    'EllipseSynergie\\ApiResponse\\Tests\\' => array($vendorDir . '/ellipsesynergie/api-response/tests'),
+    'EllipseSynergie\\ApiResponse\\' => array($vendorDir . '/ellipsesynergie/api-response/src'),
     'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
     'ClassPreloader\\' => array($vendorDir . '/classpreloader/classpreloader/src'),
     'Carbon\\' => array($vendorDir . '/nesbot/carbon/src/Carbon'),
---
+++ vendor/ellipsesynergie/api-response/src/Laravel/ResponseServiceProvider.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace EllipseSynergie\ApiResponse\Laravel;
+
+use Request;
+use Illuminate\Support\ServiceProvider;
+use EllipseSynergie\ApiResponse\Laravel\Response;
+use League\Fractal\Manager;
+
+/**
+ * Class ResponseServiceProvider
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package EllipseSynergie\ApiResponse\Laravel
+ * @author Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
+ */
+class ResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $response = $this->bootResponse();
+        $this->registerMacro($response);
+    }
+
+    /**
+     * Boot response
+     *
+     * @return Response
+     */
+    private function bootResponse()
+    {
+        $manager = new Manager;
+
+        // If you have to customize the manager instance, like setting a custom serializer,
+        // I strongly suggest you to create your own service provider and add you manager configuration action here
+        // Here some example if you want to set a custom serializer :
+        // $manager->setSerializer(\League\Fractal\Serializer\JsonApiSerializer);
+
+        // Are we going to try and include embedded data?
+        $manager->parseIncludes(explode(',', Request::input('include')));
+
+        // Return the Response object
+        $response = new Response($manager);
+
+        //Set the response instance properly
+        $this->app->instance('EllipseSynergie\ApiResponse\Contracts\Response', $response);
+
+        return $response;
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Register response macro
+     *
+     * @deprecated We still register macro for backward compatibility, but DO NOT USE THIS MACRO ANYMORE !
+     * @param $response
+     */
+    private function registerMacro($response)
+    {
+        \Response::macro('api', function() use ($response){
+            return $response;
+        });
+    }
+}
